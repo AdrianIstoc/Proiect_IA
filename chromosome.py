23894cd83ec8a4836310d3354a736939efed17a5
Eduard975 <theblackasasin@yahoo.com>
2025-01-01 16:29:29 +0200

    edit chromosome and add population

--- chromosome.py
+++ chromosome.py
@@ -1,19 +1,23 @@
-from typing import List
-import random
+import numpy as np
 
 class Chromosome:
-
-    __rand = random.Random()
-
-    def __init__(self, nrGenes: int, genes: List[float], min: List[float], max: List[float], fitness: float):
-        self.nrGenes = nrGenes
-        self.genes = genes
+    def __init__(self, no_genes: int, genes:np.ndarray[int] = None, fitness: float = 0.0, min : int = 0, max: int = 5):
+        self.no_genes = no_genes
         self.min = min
         self.max = max
         self.fitness = fitness
 
-        for i in range(self.nrGenes):
-            self.genes[i] = self.min[i] + (self.max[i]-self.min[i]) * Chromosome.__rand.random() # intre 0 si 1
-    
+        if genes is None :
+            self.genes = np.random.randint(low = min, high = max+1, size = (no_genes, no_genes))
+        else:
+            self.genes = genes
+
     def __copy__(self):
-        return Chromosome(self.nrGenes, self.genes, self.min, self.max, self.fitness)
+        return Chromosome(self.no_genes, self.genes, self.fitness, self.min, self.max)
+    
+    def copy_from(self, other):
+        self.no_genes = other.no_genes
+        self.genes = other.genes
+        self.min = other.min
+        self.max = other.max
+        self.fitness = other.fitness
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello World")
+from chromosome import Chromosome
+
+biomes = {
+    0, # Apa 
+    1, # Plaja
+    2, # Desert
+    3, # Campie
+    4, # Padure
+    5, # Munte
+}
+
+def create_chromosome(no_genes: int = 10, min:int = 0, max:int = 5):
+    return Chromosome(no_genes=no_genes, min=min, max=max)
+
+if __name__ == "__main__":
+    population_size = 20
+    population = [create_chromosome() for _ in range(population_size)]
+
